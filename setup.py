a2c839e191871e05bcd401095f8bafbab7709975
s1lvax <s1lvax@proton.me>
2024-01-13 11:09:05 +0100

    updated gitignore and version

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="huex",
-    version="0.1.0",
+    name='huex',
+    version='0.1.1',
+    author='s1lvx',
+    author_email='silva@cfsilva.com',
+    description='A powerful and lightweight CLI based tool for the Conbee II',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/s1lvax/huex',
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
-        # List your project dependencies here
-        "requests>=2.31.0",
-        "prettytable>=3.9.0"
+        'requests',
+        'prettytable',
     ],
     entry_points={
-        "console_scripts": [
-            "huex=huex.main:main",  # Adjust the path according to your project structure
+        'console_scripts': [
+            'huex=huex.main:main',
         ],
     },
-    
-    author="s1lvx",
-    author_email="silva@cfsilva.com",
-    description="Powerful, lightweight CLI tool to control Hue Lights in Conbee II",
-    keywords="hue lights, conbee II, CLI",
-    url="https://github.com/s1lvax/huex",  # Project home page
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+    ],
+    keywords='cli conbee2 zigbee huelights hue',
 )
